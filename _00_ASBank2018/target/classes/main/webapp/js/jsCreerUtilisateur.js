ea0539b181374af7e177c4e94a67e75ec1d2ad29
Raider472 <gabrielnae1@gmail.com>
2023-09-19 15:59:03 +0200

    Revert Fichier source

--- _00_ASBank2018/target/classes/main/webapp/js/jsCreerUtilisateur.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("input:radio").change(function() {
-		if ($("input[type='radio'][name='client']:checked").val()=='true'){
-			$(":text[name='numClient']").prop('disabled', false);
-		} else {
-			$(":text[name='numClient']").prop('disabled', true);
-		}
-	});
-});
